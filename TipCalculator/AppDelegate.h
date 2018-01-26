5c5db4444da72f133c656006246fa7b48daf6366
yongwoohuh <yongwoo.huh@me.com>
2018-01-26 15:06:41 -0500

    first commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Yongwoo Huh on 2018-01-26.
+//  Copyright © 2018 YongwooHuh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
